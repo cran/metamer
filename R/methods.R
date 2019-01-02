51d07841111e5199ed7b581c01a7625bb49419a6
Elio Campitelli <elio.campitelli@cima.fcen.uba.ar>
2019-01-02 22:50:03 +0000

    version 0.1.0

---
+++ R/methods.R
@@ -0,0 +1,17 @@
+#' @export
+as.data.frame.metamer_list <- function(x, ...) {
+  n <- nrow(x[[1]])
+  df <- do.call(rbind, x)
+  df$.metamer <- rep(1:length(x), each = n)
+  df$.name <- rep(attr(x, "name"), each = n)
+  lc <- length(colnames(df))
+  df <- df[, c(lc, 1:(lc-1))]
+  attr(df, "convergence") <- attr(x, "convergence")
+  return(df)
+}
+
+
+#' @export
+print.metamer_list <- function(x, ...) {
+  cat("List of ", length(x), " metamers", sep = "")
+}
